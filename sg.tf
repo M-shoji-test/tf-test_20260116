5f3b82ee7c7b2f5d27ff20263d5a228166ad76dd
shoji@nenomedia.jp <shoji@nenomedia.jp>
2026-02-18 15:58:43 +0900

    update by dev 0218.2026

--- sg.tf
+++ sg.tf
@@ -32,3 +32,41 @@ resource "aws_security_group" "web" {
   }
 }
 
+#resource "aws_security_group" "db" {
+#  name   = "WP-test-db-sg"
+#  vpc_id = aws_vpc.main.id
+#
+#  ingress {
+#    from_port       = 3306
+#    to_port         = 3306
+#    protocol        = "tcp"
+#    security_groups = [aws_security_group.web.id]
+#  }
+#
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
+
+#resource "aws_security_group" "bk" {
+#  name   = "WP-test-bk-sg"
+#  vpc_id = aws_vpc.main.id
+#
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
+
