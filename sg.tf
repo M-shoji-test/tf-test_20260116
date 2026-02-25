b4f15d7bf8fefd6d4493538a52de81cb0edcfe88
shoji@nenomedia.jp <shoji@nenomedia.jp>
2026-02-25 15:35:38 +0900

    update by.0225,2026

--- sg.tf
+++ sg.tf
@@ -37,6 +37,13 @@ resource "aws_security_group" "db" {
   vpc_id = aws_vpc.main.id
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
