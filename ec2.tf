90264374ebda7189cbe171027487690cdbf421cb
shoji@nenomedia.jp <shoji@nenomedia.jp>
2026-03-26 13:45:46 +0900

    update by 03262026 Lv4

--- ec2.tf
+++ ec2.tf
@@ -24,26 +24,26 @@ resource "aws_instance" "al2023" {
   }
 }
 
-resource "aws_instance" "db" {
-  ami                    = "ami-0677aa126dc453424"
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.private_a.id
-  key_name               = "2026-0105-test"
-  vpc_security_group_ids = [aws_security_group.db.id]
-
-  tags = { Name = "WP-test-db" }
-
-  lifecycle {
-    ignore_changes = [
-      ami,
-      user_data,
-      user_data_replace_on_change,
-      root_block_device,
-      metadata_options,
-      credit_specification
-    ]
-  }
-}
+#resource "aws_instance" "db" {
+#  ami                    = "ami-0677aa126dc453424"
+#  instance_type          = "t3.micro"
+#  subnet_id              = aws_subnet.private_a.id
+#  key_name               = "2026-0105-test"
+#  vpc_security_group_ids = [aws_security_group.db.id]
+#
+#  tags = { Name = "WP-test-db" }
+#
+#  lifecycle {
+#    ignore_changes = [
+#      ami,
+#      user_data,
+#      user_data_replace_on_change,
+#      root_block_device,
+#      metadata_options,
+#      credit_specification
+#    ]
+#  }
+#}
 
 resource "aws_instance" "bk" {
   ami                    = "ami-0c83cb1c664994bbd"
@@ -63,6 +63,37 @@ resource "aws_instance" "bk" {
       metadata_options,
       credit_specification
     ]
+  }
+}
+
+resource "aws_db_instance" "wordpress" {
+  identifier = "database-test-sj"
+
+  engine         = "mysql"
+  instance_class = "db.m7g.large"
+
+  allocated_storage     = 20
+  max_allocated_storage = 1000
+  storage_type          = "gp2"
+  storage_encrypted     = true
+
+  username = "admin"
+  password = var.db_password
+
+  port                          = 3306
+  multi_az                      = true
+  publicly_accessible           = false
+  backup_retention_period       = 7
+  monitoring_interval           = 60
+  performance_insights_enabled  = true
+  copy_tags_to_snapshot         = true
+  skip_final_snapshot           = true
+
+  db_subnet_group_name   = aws_db_subnet_group.rds_private.name
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+
+  lifecycle {
+    ignore_changes = [password]
   }
 }
 
--- sg.tf
+++ sg.tf
@@ -17,13 +17,6 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 53
-    to_port     = 53
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -32,16 +25,17 @@ resource "aws_security_group" "web" {
   }
 }
 
-resource "aws_security_group" "db" {
-  name   = "db-sg"
-  vpc_id = aws_vpc.main.id
+resource "aws_security_group" "db_sg" {
+  name        = "db-sg"
+  description = "Managed by Terraform"
+  vpc_id      = "vpc-04e25da47b1e30726"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ingress {
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   ingress {
     from_port   = 3306
@@ -76,4 +70,18 @@ resource "aws_security_group" "bk" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_db_subnet_group" "rds_private" {
+  name = "default-vpc-04e25da47b1e30726"
+
+  subnet_ids = [
+    "subnet-020b02d4053409608",
+    "subnet-0bea03c8892a85559",
+    "subnet-0bf7b7520cb75fda5",
+    "subnet-0c606d5acf9e1171c",
+  ]
+
+  description = "Created from the RDS Management Console"
+}
+
 
